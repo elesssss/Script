3a266abc4e59f573b9273fffc110ecc15004510f
elesssss <162124503+elesssss@users.noreply.github.com>
2024-04-22 02:00:43 +0800

    Update shell.sh

--- script/shell.sh
+++ script/shell.sh
@@ -79,7 +79,7 @@ check_pmc(){
         updates="yum update -y"
         installs="yum install -y"
         check_install="rpm -q"
-        apps=("cronie" "net-tools" "iproute" "python3")
+        apps=("cronie" "net-tools" "iproute" "python38")
     elif [[ "$release" == "alpine" ]]; then
         updates="apk update"
         installs="apk add"
