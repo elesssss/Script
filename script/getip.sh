111639e1589df7e780030aa61b3c364bb318bb0f
elesssss <162124503+elesssss@users.noreply.github.com>
2024-09-24 16:57:27 +0800

    Create getip.sh

---
+++ script/getip.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+Red="\033[31m" # 红色
+Green="\033[32m" # 绿色
+Yellow="\033[33m" # 黄色
+Blue="\033[34m" # 蓝色
+Nc="\033[0m" # 重置颜色
+Red_globa="\033[41;37m" # 红底白字
+Green_globa="\033[42;37m" # 绿底白字
+Yellow_globa="\033[43;37m" # 黄底白字
+Blue_globa="\033[44;37m" # 蓝底白字
+Info="${Green}[信息]${Nc}"
+Error="${Red}[错误]${Nc}"
+Tip="${Yellow}[提示]${Nc}"
+start_time=$(date +%s) # 记录开始时间
+end_time=$(date +%s) # 记录结束时间
+duration=$((end_time - start_time)) # 计算持续时间
+
+# 检查是否为root用户
+check_root(){
+    if [ "$(id -u)" != "0" ]; then
+        echo -e "${Error} 当前非ROOT账号(或没有ROOT权限)，无法继续操作，请更换ROOT账号或使用 ${Green_globa}sudo -i${Nc} 命令获取临时ROOT权限（执行后可能会提示输入当前账号的密码）。"
+        exit 1
+    fi
+}
+
+check_release(){
+    if [[ -e /etc/os-release ]]; then
+        . /etc/os-release
+        release=$ID
+    elif [[ -e /usr/lib/os-release ]]; then
+        . /usr/lib/os-release
+        release=$ID
+    fi
+    os_version=$(echo $VERSION_ID | cut -d. -f1,2)
+
+    if [[ "${release}" == "ol" ]]; then
+        release=oracle
+    elif [[ ! "${release}" =~ ^(kali|centos|ubuntu|fedora|debian|almalinux|rocky|alpine)$ ]]; then
+        echo -e "${Error} 抱歉，此脚本不支持您的操作系统。"
+        echo -e "${Info} 请确保您使用的是以下支持的操作系统之一："
+        echo -e "-${Red} Ubuntu ${Nc}"
+        echo -e "-${Red} Debian ${Nc}"
+        echo -e "-${Red} CentOS ${Nc}"
+        echo -e "-${Red} Fedora ${Nc}"
+        echo -e "-${Red} Kali ${Nc}"
+        echo -e "-${Red} AlmaLinux ${Nc}"
+        echo -e "-${Red} Rocky Linux ${Nc}"
+        echo -e "-${Red} Oracle Linux ${Nc}"
+        echo -e "-${Red} Alpine Linux ${Nc}"
+        exit 1
+    fi
+}
+
+check_pmc(){
+    check_release
+    if [[ "$release" == "debian" || "$release" == "ubuntu" || "$release" == "kali" ]]; then
+        updates="apt update -y"
+        installs="apt install -y"
+        check_install="dpkg -s"
+        apps=("net-tools")
+    elif [[ "$release" == "alpine" ]]; then
+        updates="apk update -f"
+        installs="apk add -f"
+        check_install="apk info -e"
+        apps=("net-tools")
+    elif [[ "$release" == "almalinux" || "$release" == "rocky" || "$release" == "oracle" ]]; then
+        updates="dnf update -y"
+        installs="dnf install -y"
+        check_install="dnf list installed"
+        apps=("net-tools")
+    elif [[ "$release" == "centos" ]]; then
+        updates="yum update -y"
+        installs="yum install -y"
+        check_install="yum list installed"
+        apps=("net-tools")
+    elif [[ "$release" == "fedora" ]]; then
+        updates="dnf update -y"
+        installs="dnf install -y"
+        check_install="dnf list installed"
+        apps=("net-tools")
+    fi
+}
+
+install_base(){
+    check_pmc
+    cmds=("netstat")
+    echo -e "${Info} 你的系统是${Red} $release $os_version ${Nc}"
+    echo
+
+    for g in "${!apps[@]}"; do
+        if ! $check_install "${apps[$g]}" &> /dev/null; then
+            CMDS+=(${cmds[g]})
+            DEPS+=("${apps[$g]}")
+        fi
+    done
+    
+    if [ ${#DEPS[@]} -gt 0 ]; then
+        $updates &> /dev/null
+        $installs "${DEPS[@]}" &> /dev/null
+    fi
+}
+
+get_public_ip(){
+    InFaces=($(netstat -i | awk '{print $1}' | grep -E '^(eth|ens|eno|esp|enp|venet|vif)'))
+
+    for i in "${InFaces[@]}"; do # 从网口循环获取IP
+        IPv4=$(curl -s4 --max-time 2 --interface "$i" ip.gs)
+        IPv6=$(curl -s6 --max-time 2 --interface "$i" ip.gs)
+
+        if [[ -n "$IPv4" || -n "$IPv6" ]]; then # 检查是否获取到IP地址
+            break # 获取到任一IP类型停止循环
+        fi
+    done
+}
+
+Echo_IP(){
+    get_public_ip
+    echo -e "${Info} IPv4 是${Green} ${IPv4}${Nc}"
+    echo -e "${Info} IPv6 是${Green} ${IPv6}${Nc}"
+    echo -e "${Info} 脚本运行时间: ${Green}${duration}${Nc} 秒。"
+}
+
+main(){
+    check_root
+    install_base
+    Echo_IP
+}
+
+main
