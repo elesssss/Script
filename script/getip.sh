07aed10efe916146b3dbe1955eec1f9c5c3fc991
elesssss <162124503+elesssss@users.noreply.github.com>
2024-10-11 22:33:28 +0800

    Update getip.sh

--- script/getip.sh
+++ script/getip.sh
@@ -101,7 +101,7 @@ install_base(){
 }
 
 get_public_ip(){
-    InFaces=($(netstat -i | awk '{print $1}' ))
+    InFaces=($(netstat -i | awk '{print $1}' | grep -E '^(eth|ens|eno|esp|enp|venet|veth|vif)'))
 
     for i in "${InFaces[@]}"; do # 从网口循环获取IP
         IPv4=$(curl -s4 --max-time 2 --interface "$i" ip.gs)
