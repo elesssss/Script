826bbffa570ddf460506d6124ebabff7e735b757
elesssss <162124503+elesssss@users.noreply.github.com>
2025-06-18 15:16:12 +0800

    Update op_overlay.sh

--- script/op_overlay.sh
+++ script/op_overlay.sh
@@ -15,35 +15,41 @@ if [ ! -e /etc/rootpt-resize ] && type parted >/dev/null && lock -n /var/lock/ro
         echo "分区扩展失败!"
         exit 1
     }
-    # 获取loop设备偏移量
-    OFFSET="$(losetup | awk '/\/dev\/loop0/{print $3}')"
-    if [ -n "$OFFSET" ] && [ "$OFFSET" -gt 0 ]; then
-        # 创建新loop设备
-        losetup -f -o "$OFFSET" "${ROOT_BLK}"
-        mkdir -p /mnt/resize-tmp
-        mount /dev/loop1 /mnt/resize-tmp
-        umount /dev/loop1
-    else
-        losetup /dev/loop1 "${ROOT_BLK}"
-    fi
-    # 检查文件系统类型并扩容
-    FSTYPE="$(lsblk -f | awk '/loop1/{print $2}')"
-    if [ "$FSTYPE" = "f2fs" ]; then
-        if ! resize.f2fs -f /dev/loop1; then
-            echo "f2fs扩容失败!"
-            exit 1
-        fi
-    elif [ "$FSTYPE" = "ext4" ]; then
-        if ! resize2fs -f /dev/loop1; then
-            echo "ext4扩容失败!"
-            exit 1
-        fi
-    fi
     reboot
 fi
 EOF
+
+cat << "EOF" > /etc/uci-defaults/80-rootpt-resize
+ROOT_BLK="$(fdisk -l 2>/dev/null | awk '/^\/dev/{print $1}' | tail -n 2 | head -n 1)"
+# 获取loop设备偏移量
+OFFSET="$(losetup | awk '/\/dev\/loop0/{print $3}')"
+if [ -n "$OFFSET" ] && [ "$OFFSET" -gt 0 ]; then
+    # 创建新loop设备
+    losetup -f -o "$OFFSET" "${ROOT_BLK}"
+    mkdir -p /mnt/resize-tmp
+    mount /dev/loop1 /mnt/resize-tmp
+    umount /dev/loop1
+else
+    losetup /dev/loop1 "${ROOT_BLK}"
+fi
+# 检查文件系统类型并扩容
+FSTYPE="$(lsblk -f | awk '/loop1/{print $2}')"
+if [ "$FSTYPE" = "f2fs" ]; then
+    if ! resize.f2fs -f /dev/loop1; then
+        echo "f2fs扩容失败!"
+        exit 1
+    fi
+elif [ "$FSTYPE" = "ext4" ]; then
+    if ! resize2fs -f /dev/loop1; then
+        echo "ext4扩容失败!"
+        exit 1
+    fi
+fi
+reboot
+EOF
 cat << "EOF" >> /etc/sysupgrade.conf
 /etc/uci-defaults/70-rootpt-resize
+/etc/uci-defaults/80-rootpt-resize
 EOF
 
 sh /etc/uci-defaults/70-rootpt-resize
