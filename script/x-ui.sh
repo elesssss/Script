4825637dd15b323115aa37ff607230646b6731e1
elesssss <162124503+elesssss@users.noreply.github.com>
2026-08-01 17:31:32 +0800

    Update x-ui.sh

--- script/x-ui.sh
+++ script/x-ui.sh
@@ -334,7 +334,7 @@ install_bbr(){
 
 update_shell(){
     echo -e "${Info} 正在更新管理脚本..."
-    wget -O /usr/bin/x-ui -N --no-check-certificate https://raw.githubusercontent.com/elesssss/Script/main/script/x-ui.sh
+    wget -O /usr/bin/x-ui --no-check-certificate https://raw.githubusercontent.com/elesssss/Script/main/script/x-ui.sh
     if [[ $? != 0 ]]; then
         echo ""
         echo -e "${Error} 下载脚本失败，请检查本机能否连接 Github"
